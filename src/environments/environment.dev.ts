1c4ea9f86a84377fc7bf5e762e951de3733706a0
Natalia Kosheva <natalia.kosheva@nixsolutions.com>
2022-04-27 20:30:49 +0300

    Start ignoring environments

--- src/environments/environment.dev.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  CLIENT_ID: '8cf7f20dedd44ffdbc2cef0fd7ad7b76',
-  CLIENT_SECRET: 'c5279f9b98d949208542943314d87edf',
-  ACCESS_SPOTIFY_USER_TOKEN:
-    'BQABxZ_8PEUNcPJqwGOMipJhMsXCqE5m92fru0BTWtcvZQrj-2_pGEdsXkeF6P2CRk5EO3cv5c28dfS5B5VBGJylZI4He2DqfhNvqstPlFj-0gn-RMOSnqo-aNIhoMjhaamwuaCF_Op3JwZuNP6HHHIn4qSzV7kk024jg1MR3SE-9Gnw1crkId3TaKnJQCHm106e5cO-gdVe40s4mywQh5nTk1huNLo2mIGKPCTXlqdIxBNhwy2y9HE',
-};
