d454c5e8c143f9441f78f40c9b35a49fdbc8f6d7
Natalia Kosheva <natalia.kosheva@nixsolutions.com>
2022-04-27 21:22:10 +0300

    add new translate

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,18 @@ import { HomeComponent } from 'src/app/home/home.component';
 import { CarouselModule } from 'src/app/shared/components/carousel/carousel.module';
 import { HomeRoutingModule } from 'src/app/home/home-routing.module';
 import { PlayerService } from 'src/app/shared/services/player.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, CarouselModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    CarouselModule,
+    TranslateModule.forChild({
+      extend: true,
+    }),
+  ],
   exports: [HomeComponent],
   providers: [PlayerService],
 })
