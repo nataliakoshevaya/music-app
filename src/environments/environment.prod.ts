f7e89a0b7204bbd480a01ce1a08f6c6e7cac1e35
Natalia Kosheva <natalia.kosheva@nixsolutions.com>
2022-04-27 20:07:12 +0300

    create environment folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  CLIENT_ID: '8cf7f20dedd44ffdbc2cef0fd7ad7b76',
+  CLIENT_SECRET: 'c5279f9b98d949208542943314d87edf',
+  ACCESS_SPOTIFY_USER_TOKEN:
+    'BQABxZ_8PEUNcPJqwGOMipJhMsXCqE5m92fru0BTWtcvZQrj-2_pGEdsXkeF6P2CRk5EO3cv5c28dfS5B5VBGJylZI4He2DqfhNvqstPlFj-0gn-RMOSnqo-aNIhoMjhaamwuaCF_Op3JwZuNP6HHHIn4qSzV7kk024jg1MR3SE-9Gnw1crkId3TaKnJQCHm106e5cO-gdVe40s4mywQh5nTk1huNLo2mIGKPCTXlqdIxBNhwy2y9HE',
+};
